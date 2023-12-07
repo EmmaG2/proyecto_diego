0ffffa2fd115e3cd0ea77764df58a9ac296534bb
EmmaG2 <itsmeemmadev@gmail.com>
2023-12-06 20:18:56 -0600

    Se aplica el concepto de encapsulación en la clase

--- src/lib/clases/Persona.py
+++ src/lib/clases/Persona.py
@@ -2,37 +2,37 @@ class Persona:
     def __init__(
         self, nombre, estado_civil, apellido, edad, escolaridad, promedio, salario
     ):
-        self.nombre = nombre
-        self.estado_civil = estado_civil
-        self.apellido = apellido
-        self.edad = edad
-        self.escolaridad = escolaridad
-        self.promedio = promedio
-        self.salario = salario
+        self.__nombre = nombre
+        self.__estado_civil = estado_civil
+        self.__apellido = apellido
+        self.__edad = edad
+        self.__escolaridad = escolaridad
+        self.__promedio = promedio
+        self.__salario = salario
         
     def get_nombre(self):
-        return self.nombre
+        return self.__nombre
 
     def get_estado_civil(self):
-        return self.estado_civil
+        return self.__estado_civil
 
     def get_apellido(self):
-        return self.apellido
+        return self.__apellido
 
     def get_edad(self):
-        return self.edad
+        return self.__edad
 
     def get_escolaridad(self):
-        return self.escolaridad
+        return self.__escolaridad
 
     def get_promedio(self):
-        return self.promedio
+        return self.__promedio
 
     def get_salario(self):
-        return self.salario
+        return self.__salario
     
     def __str__(self):
         return (
-            f"Persona(nombre={self.nombre}, estado_civil={self.estado_civil}, apellido={self.apellido}, "
-            f"edad={self.edad}, escolaridad={self.escolaridad}, promedio={self.promedio}, salario={self.salario})"
+            f"Persona(nombre={self.__nombre}, estado_civil={self.__estado_civil}, apellido={self.__apellido}, "
+            f"edad={self.__edad}, escolaridad={self.__escolaridad}, promedio={self.__promedio}, salario={self.__salario})"
         )
